3be9c851d37e9a768337636d7eac715f93a01329
Albert <albertsimanjuntak02@gmail.com>
2020-03-26 09:46:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.geoquiz
+
+
+import androidx.lifecycle.ViewModel
+
+class QuizViewModel : ViewModel() {
+
+    private val questionBank= listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, true),
+        Question(R.string.question_africa, true),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    var currentIndex = 0;
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() =  questionBank[currentIndex].textResId
+
+    fun moveToNext(){
+        currentIndex = (currentIndex+1) % questionBank.size
+    }
+}
